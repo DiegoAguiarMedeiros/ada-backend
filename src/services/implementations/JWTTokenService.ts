6e5d8eefe20d5ca4f41250d882850fadb393efa7
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2023-09-16 16:19:54 -0300

    fix: get JWT_SECRET from env

--- src/services/implementations/JWTTokenService.ts
+++ src/services/implementations/JWTTokenService.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { ITokenService } from '../ITokenService';
 
 const oneDayInSeconds = 86400;
-const PRIVATE_KEY = 'mytm_2020';
+const PRIVATE_KEY = process.env.JWT_SECRET;
 export default class JWTTokenService implements ITokenService {
   sign(payload: unknown): string {
     return jwt.sign(payload, PRIVATE_KEY, { expiresIn: oneDayInSeconds });
